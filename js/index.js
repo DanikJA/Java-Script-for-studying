586338e7bc28dc4cfbd0bf34d02fc32c1a5fa844
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-06 18:28:45 +0300

    added variables

---
+++ js/index.js
@@ -0,0 +1,24 @@
+        //    Creating variables
+
+document.write("Hello");
+ document.write("!!!");
+ console.error('hello java script');
+console.warn('!!!');
+ 
+// let num = 5;
+// console.log(num);
+
+// num = 7;
+// console.log(num);
+
+let num = 6;
+console.log("Result:" + num + '!');
+
+const number = 10;
+
+let isJohnHasCar = false;
+
+
+
+
+        

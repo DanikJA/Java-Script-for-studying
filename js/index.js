1f83797756ce6fc29b4f5b53dd0a9d72d325de0c
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-08 18:08:11 +0300

    several exercises  to practice JavaScript

--- js/index.js
+++ js/index.js
@@ -74,4 +74,74 @@ const totalPrice = (pricePerDroid * orderedQuantity + deliveryFee);
 const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
 
 
+
+
+// let result;
         
+// function getAge() {
+//   let currentYear = 2024;
+//   let birthdayYear = 2001;
+//   result = currentYear - birthdayYear;
+// }
+
+// getAge()
+//   console.log(result);
+
+
+
+function getAge(year = 0) {
+  let currentYear = 2024;
+  return currentYear - year;
+}
+
+console.log(getAge(2001));
+
+
+
+
+
+// Оголошена функція makeMessage(name, price)
+// Виклик makeMessage('Radar', 6150) повертає "You picked Radar, price per item is 6150 credits"
+// Виклик makeMessage('Scanner', 3500) повертає "You picked Scanner, price per item is 3500 credits"
+// Виклик makeMessage('Reactor', 8000) повертає "You picked Reactor, price per item is 8000 credits"
+// Виклик makeMessage('Engine', 4070) повертає "You picked Engine, price per item is 4070 credits"
+
+
+function makeMessage (name, price) {
+  // Change code below this line
+   const message = `You picked ${name}, price per item is ${price} credits`;
+  // Change code above this line
+  return message;
+};
+
+
+console.log(makeMessage('Radar', 6150));
+
+console.log(makeMessage('Scaner', 3500));
+
+console.log(makeMessage('Engine', 4070));
+
+
+
+// Доповни код функції таким чином, щоб вона повертала повідомлення про замовлення у форматі "You ordered droids worth <total price> credits. Delivery (<delivery fee> credits) is included in total price.". Не забудь про ціну доставки в обчисленнях загальної вартості.
+
+// Оголошена функція makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee)
+// Виклик makeOrderMessage(2, 100, 50) повертає "You ordered droids worth 250 credits. Delivery (50 credits) is included in total price."
+// Виклик makeOrderMessage(4, 300, 100) повертає "You ordered droids worth 1300 credits. Delivery (100 credits) is included in total price."
+// Виклик makeOrderMessage(10, 70, 200) повертає "You ordered droids worth 900 credits. Delivery (200 credits) is included in total price."
+
+
+function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+  // Change code below this line
+  
+  const totalPrice =  orderedQuantity * pricePerDroid + deliveryFee;
+  
+  const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+
+  // Change code above this line
+  return message;
+};
+
+console.log(makeOrderMessage(2,100,50));
+console.log(makeOrderMessage(4,300,100));
+console.log(makeOrderMessage(10,70,20));

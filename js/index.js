0b44b410ce24e8cf48272a3181d096a6c9d61da5
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-12 15:22:52 +0300

    Logical operators

--- js/index.js
+++ js/index.js
@@ -310,4 +310,24 @@ console.log((100, 130));
 console.log((70, 0));
 console.log((200, 20));
 console.log((200, 250));
-console.log((150,0));
+console.log((150, 0));
+
+
+
+// Функція isNumberInRange(start, end, number) перевіряє, чи входить число у проміжок. Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+
+// number - число, входження якого перевіряється
+// start - початок числового проміжку
+// end - кінець числового проміжку
+// Присвой змінній isInRange вираз перевірки входження number у числовий проміжок від start до end. Тобто число повинно бути більшим або дорівнювати start, і меншим або дорівнювати end. Результатом виразу перевірки буде буль true або false.
+
+function isNumberInRange(start, end, number) {
+  const isInRange = (number >= start && number <= end); // Change this line
+  
+  return isInRange;
+}
+
+console.log(isNumberInRange(10, 30, 17));
+console.log(isNumberInRange(10, 30, 5));
+console.log(isNumberInRange(20, 50, 24));
+console.log(isNumberInRange(20, 50, 76));

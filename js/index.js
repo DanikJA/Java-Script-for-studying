4e389dd2fa6acf775e3c89cd41fd5dbbef585409
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-13 15:19:29 +0300

    practice

--- js/index.js
+++ js/index.js
@@ -331,3 +331,44 @@ console.log(isNumberInRange(10, 30, 17));
 console.log(isNumberInRange(10, 30, 5));
 console.log(isNumberInRange(20, 50, 24));
 console.log(isNumberInRange(20, 50, 76));
+
+
+
+
+
+
+
+
+
+
+function getDiscount(totalSpent) {
+  const BASE_DISCOUNT = 0;
+  const BRONZE_DISCOUNT = 0.02;
+  const SILVER_DISCOUNT = 0.05;
+  const GOLD_DISCOUNT = 0.1;
+  let discount;
+  // Change code below this line
+  
+  if (totalSpent >= 50000){
+    discount =`знижка 10% ${GOLD_DISCOUNT}`
+  } else if(totalSpent >= 20000 && totalSpent < 50000){
+    discount =`знижка 5% ${SILVER_DISCOUNT}`
+  } else if(totalSpent >= 5000 && totalSpent < 20000){
+    discount =`знижка 2% ${BRONZE_DISCOUNT}`
+  }else if (totalSpent < 5000){
+   discount = `знижка 0% ${BASE_DISCOUNT}`
+  }
+  
+  // Change code above this line
+  return discount;
+  discount = Number(discount);
+}
+
+console.log(getDiscount(137000));
+console.log(getDiscount(46900));
+console.log(getDiscount(8250));
+console.log(getDiscount(1300));
+console.log(getDiscount(5000));
+console.log(getDiscount(20000));
+console.log(getDiscount(50000));
+

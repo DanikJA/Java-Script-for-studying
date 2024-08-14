1808b92b6097a7f14c4b20c77c1362ad8fbfc12c
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-14 13:14:24 +0300

    arrays and functions

--- js/index.js
+++ js/index.js
@@ -646,3 +646,16 @@ console.log(checkForSpam("Trust me, this is not a spam message"));
 console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
 console.log(checkForSpam("[SPAM] How to earn fast money?"));
 
+
+
+
+
+
+
+
+                            // arrays and functions
+
+
+
+
+      

09d04b7498c2df5132da559965fb95b6ce4ad5c2
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-16 18:42:44 +0300

    ...

--- js/index.js
+++ js/index.js
@@ -763,6 +763,29 @@ const lastElementIndex = fruits.length -1;
 const lastElement = fruits[lastElementIndex];
 
 
+// 9.
+
+// function getExtremeElements(array) {
+//   // Change code below this line
+// return  [array[0], array[array.length -1]];
+
+//   // Change code above this line
+// }
 
 
 
+// console.log(getExtremeElements([1, 2, 3, 4, 5]));
+// console.log(getExtremeElements(["Earth", "Mars", "Venus"]));
+// console.log(getExtremeElements(["apple", "peach", "pear", "banana"]));
+
+
+function getExtremeElements(array) {
+  // Change code below this line
+return [array[0], array[array.length -1]];
+
+  // Change code above this line
+}
+
+console.log(getExtremeElements([1, 2, 3, 4, 5]));
+console.log(getExtremeElements(["Earth", "Mars", "Venus"]));
+console.log(getExtremeElements(["apple", "peach", "pear", "banana"]));

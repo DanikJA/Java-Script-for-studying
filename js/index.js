a6669088a231b3085609d0224aec4cbea6a946eb
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-16 16:02:18 +0300

    a few new ones

--- js/index.js
+++ js/index.js
@@ -661,3 +661,55 @@ console.log(checkForSpam("[SPAM] How to earn fast money?"));
 
 
       
+// const job = function (item) {
+//   for (const item of items) {
+//     console.log(itme);
+//   }
+// }
+
+// 1.
+
+function checkAge(age) {
+  if (age >= 18) { // Change this line
+    return "You are an adult";
+  }
+
+  return "You are a minor";
+}
+
+// 2.
+
+function checkPassword(password) {
+  const ADMIN_PASSWORD = "jqueryismyjam";
+
+  if (password === ADMIN_PASSWORD) {
+    return  "Welcome!";
+  } 
+    return  "Access denied, wrong password!";
+}
+
+console.log(checkPassword('mangohackzor'));
+console.log(checkPassword('polyhax'));
+console.log(checkPassword('jqueryismyjam'));
+
+
+// 3.
+
+function checkStorage(available, ordered) {
+  // Change code below this line
+  
+  if (ordered === 0) {
+    return "Your order is empty!";
+  } if (ordered > available) {
+    return "Your order is too large, not enough goods in stock!";
+  } 
+  return "The order is accepted, our manager will contact you";
+  
+}
+
+
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+console.log(checkStorage(70, 0));
+console.log(checkStorage(200, 250));
+console.log(checkStorage(150, 0));

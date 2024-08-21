e12976d8ab8df10458c223e643f9edea74999571
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-21 15:09:38 +0300

    worked with array, type include()

--- js/index.js
+++ js/index.js
@@ -1029,3 +1029,43 @@ console.log(createArrayOfNumbers(29, 34));
 
 
 
+// 23
+
+function filterArray(numbers, value) {
+   // Change code below this line
+
+const newArray = [];
+
+  for(let i = 0; i < numbers.length; i += 1){
+    if (numbers[i] > value){
+      newArray.push(numbers[i]);
+    }
+  }
+
+  return newArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
+
+
+// 24.
+
+function checkFruit(fruit) {
+  const fruits = ["apple", "plum", "pear", "orange"];
+ const checkArrayForAvailability = fruits.includes(fruit);
+  
+  return checkArrayForAvailability; // Change this line
+}
+
+console.log(checkFruit('plum'));
+console.log(checkFruit('mandarin'));
+console.log(checkFruit('pear'));
+console.log(checkFruit('apple'));
+
+

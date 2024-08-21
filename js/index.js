0dc4910f41694805e717725ce3f91dfe54be29ea
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-21 16:49:27 +0300

    for of

--- js/index.js
+++ js/index.js
@@ -1122,3 +1122,31 @@ console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
 console.log(calculateTotalPrice([]));
 
 
+// 27
+
+function filterArray(numbers, value) {
+  // Change code below this line
+  const filteredNumbers = [];
+
+  for (const number of numbers) {
+
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
+
+  return filteredNumbers;
+  // Change code above this line
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
+// 28.
+
+

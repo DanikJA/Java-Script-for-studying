4c191095c8c0ca1dc51afe294be15a974b4ffb1d
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-21 18:05:35 +0300

    added the operator return to the exercise

--- js/index.js
+++ js/index.js
@@ -1195,4 +1195,23 @@ console.log(getEvenNumbers(7, 7));
 // }
 
 
-31.
+// 31.
+
+
+function findNumber(start, end, divisor) {
+  // Change code below this line
+ 
+  for (let i = start; i < end; i += 1) {
+    if (i % divisor === 0) {
+      return i;
+    }
+  }
+  // Change code above this line
+}
+
+console.log(findNumber(2, 6, 5));
+console.log(findNumber(8, 17, 3));
+console.log(findNumber(6, 9, 4));
+console.log(findNumber(16, 35, 7));
+
+

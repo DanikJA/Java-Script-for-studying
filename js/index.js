f5969ef6a461e68db82fd1d5eb47b3e1d7d6217e
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-23 19:52:03 +0300

    added the new object

--- js/index.js
+++ js/index.js
@@ -1245,11 +1245,33 @@ console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
 
                     //  Objects 
                  
-                    
+
+// const apartment = {
+// imgUrl:"https://via.placeholder.com/640x480",
+// descr:"Spacious apartment in the city center",
+// rating: 4,
+// price: 2153,
+// tags: ["premium", "promoted", "top"],
+// };
+
+
+
+
+
+
+// // 2.
+
 const apartment = {
-imgUrl:"https://via.placeholder.com/640x480",
-descr:"Spacious apartment in the city center",
-rating: 4,
-price: 2153,
-tags: ["premium", "promoted", "top"],
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  
+  owner: {
+       name: "Henry",
+       phone: "982-126-1588",
+       email: "henry.carter@aptmail.com",
+    
+  }
 };

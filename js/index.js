5130faaac8b9220b7a13447b2ec58240abc77603
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-24 23:05:25 +0300

    ...

--- js/index.js
+++ js/index.js
@@ -1261,17 +1261,40 @@ console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
 
 // // 2.
 
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+  
+//   owner: {
+//        name: "Henry",
+//        phone: "982-126-1588",
+//        email: "henry.carter@aptmail.com",
+    
+//   }
+// };
+
+
+// 3.
+
 const apartment = {
   imgUrl: "https://via.placeholder.com/640x480",
   descr: "Spacious apartment in the city center",
   rating: 4,
   price: 2153,
   tags: ["premium", "promoted", "top"],
-  
-  owner: {
-       name: "Henry",
-       phone: "982-126-1588",
-       email: "henry.carter@aptmail.com",
-    
-  }
 };
+
+// Change code below this line
+const aptRating = apartment.rating;
+const aptDescr = apartment.descr;
+const aptPrice = apartment.price;
+const aptTags = apartment.tags;
+// Change code above this line
+
+
+// 4.
+
+

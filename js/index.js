52cf67a37ce21602892848c63c0e201e2cfa8671
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-28 21:16:24 +0300

    worked with an object

--- js/index.js
+++ js/index.js
@@ -1298,3 +1298,46 @@ const aptTags = apartment.tags;
 // 4.
 
 
+// const temp = [0, 3, 4, 5, 6, 7];
+
+// const copy = [...temp];
+// console.log(temp)
+
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// const ownerName = apartment.owner.name;
+// console.log(ownerName);
+
+// const ownerPhone = apartment.owner.phone;
+// console.log(ownerPhone);
+
+// const ownerEmail = apartment.owner.email;
+// console.log(ownerEmail);
+
+// const numberOfTags = apartment.tags.length;
+// console.log(numberOfTags);
+
+// const firstTag = apartment.tags[0];
+// console.log(firstTag);
+
+// const lastTag = apartment.tags[apartment.tags.length -1];
+// console.log(lastTag);
+
+// // Change code above this line
+
+
+
+

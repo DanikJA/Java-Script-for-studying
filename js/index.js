67e4ce35f59451506219d4b9095a2353c70539ed
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-29 14:04:24 +0300

    updates

--- js/index.js
+++ js/index.js
@@ -1381,3 +1381,57 @@ const aptTags = apartment.tags;
 // apartment.tags.push("trusted");
 
 
+// 7.
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {
+//   country: "Jamaica",
+//   city: "Kingston",
+// }
+
+
+// 8.
+
+// const name = "Repair Droid";
+// const price = 2500;
+// const image = "https://via.placeholder.com/640x480";
+// const tags = ["on sale", "trending", "best buy"];
+
+// const product = {
+//   name,
+//   price,
+//   image,
+//   tags,
+// };
+
+
+
+// 9.
+
+
+const emailInputName = "email";
+const passwordInputName = "password";
+
+const credentials = {
+  // Change code below this line
+
+[emailInputName]: "henry.carter@aptmail.com",
+[passwordInputName]: "jqueryismyjam"
+
+  // Change code above this line
+};
+

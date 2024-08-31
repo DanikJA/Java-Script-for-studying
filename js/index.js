1708fcad47f627ee727fb7ecb0210d27b4b9edf7
Danik2023 <danik.kyrteu2@gmail.com>
2024-08-31 13:26:38 +0300

    ...

--- js/index.js
+++ js/index.js
@@ -1581,3 +1581,28 @@ for(const color of colors){
 
 18.
 
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+ 
+  const productPrice = {};
+
+function getProductPrice(productName) {
+
+  for(const product of products){
+  if(productName === product.name){
+    productPrice[productName] = product.price;
+    return product.price; 
+    } 
+  }
+  return null;
+}
+
+console.log(getProductPrice("Radar"));
+console.log(getProductPrice("Grip"));
+console.log(getProductPrice("Scanner"));
+console.log(getProductPrice("Droid"));
+console.log(getProductPrice("Engine"));

540810005f2fa804503441a6305d5ed0e0678ace
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-02 19:54:52 +0300

    calculating

--- js/index.js
+++ js/index.js
@@ -1637,3 +1637,31 @@ console.log(getProductPrice("Engine"));
 // console.log(getAllPropValues("quantity"));
 // console.log(getAllPropValues("price"));
 // console.log(getAllPropValues("category"));
+
+
+20.
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//  let totalPrice = 0;
+  
+// for(product of products){
+//   if(product.name === productName){
+//    totalPrice = product.price * product.quantity;
+//     break;
+//   }
+// }
+//   return totalPrice;
+// }
+
+// console.log(calculateTotalPrice("Blaster"));
+// console.log(calculateTotalPrice("Radar"));
+// console.log(calculateTotalPrice("Droid"));
+// console.log(calculateTotalPrice("Grip"));
+// console.log(calculateTotalPrice("Grip"));

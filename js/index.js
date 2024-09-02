c0d0d8af643f3c459ac4d9b18f36a27661448c58
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-02 20:45:15 +0300

    ...

--- js/index.js
+++ js/index.js
@@ -1679,3 +1679,23 @@ const {yesterday, today, tomorrow} = highTemperatures;
 
 // Change code above this line
 const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+
+// 22.
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const {yesterday, 
+//        today, 
+//        tomorrow,
+//        icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",} = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+

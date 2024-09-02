d4cab187823df1c300bd223e807cbb11b0a0c843
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-02 20:35:33 +0300

    ...

--- js/index.js
+++ js/index.js
@@ -1665,3 +1665,17 @@ console.log(getProductPrice("Engine"));
 // console.log(calculateTotalPrice("Droid"));
 // console.log(calculateTotalPrice("Grip"));
 // console.log(calculateTotalPrice("Grip"));
+
+21.
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+
+const {yesterday, today, tomorrow} = highTemperatures;
+
+
+// Change code above this line
+const meanTemperature = (yesterday + today + tomorrow) / 3;

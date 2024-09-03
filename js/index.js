677b17861eef763916eca9e186cc84eb2bfc0404
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-03 21:44:40 +0300

    one more function

--- js/index.js
+++ js/index.js
@@ -1786,3 +1786,29 @@ const overrideSettings = {
 // Change code below this line
 const finalSettings = {...defaultSettings, ...overrideSettings};
 
+
+// 29.
+
+
+function makeTask(data) {
+  const completed = false;
+  const category = "General";
+  const priority = "Normal";
+  // Change code below this line
+ const newObject = {
+   completed,
+   category,
+   priority,
+   ...data };
+  // Change code above this line
+
+  return newObject;
+}
+
+
+console.log(makeTask({ category: "Homemade", priority: "Low", text: "Take out the trash" }));
+console.log(makeTask({ category: "Finance", text: "Take interest" }));
+console.log(makeTask({ priority: "Low", text: "Choose shampoo" }));
+console.log(makeTask({ text: "Buy bread" }));
+
+

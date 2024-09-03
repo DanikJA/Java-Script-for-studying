83f4e9779f045c09b41d1de8561089b94e8049df
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-03 21:18:28 +0300

    ...

--- js/index.js
+++ js/index.js
@@ -1770,3 +1770,19 @@ const worstScore = Math.min(...scores);
 
 // 28
 
+const defaultSettings = {
+  theme: "light",
+  public: true,
+  withPassword: false,
+  minNumberOfQuestions: 10,
+  timePerQuestion: 60,
+};
+const overrideSettings = {
+  public: false,
+  withPassword: true,
+  timePerQuestion: 30,
+};
+
+// Change code below this line
+const finalSettings = {...defaultSettings, ...overrideSettings};
+

7813ed25e0445db9b80db2d04b39a498724fee34
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-03 21:14:19 +0300

    ...

--- js/index.js
+++ js/index.js
@@ -1759,4 +1759,14 @@ const worstScore = Math.min(...scores);
 
 27.//
 
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
+
+
+// 28
 

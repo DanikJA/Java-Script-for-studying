f8ef954bfd7a7575ca25dc2f8c02aea93aad301e
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-04 12:58:18 +0300

    added the new pretty difficult  function

--- new.js
+++ new.js
@@ -1,10 +1,40 @@
 
-function multiply(firstNumber, secondNumber, ...otherArgs) {
-  console.log(firstNumber); // Значення першого аргументу
-  console.log(secondNumber); // Значення другого аргументу
-  console.log(otherArgs); // Масив інших аргументів
+// Change code below this line
+function add(...args) {
+  const values = Object.values(args);
+  let total = 0;
+ for(value of values){
+   total += value;
+ }
+  return total;
 }
 
-multiply(1, 2);
-multiply(1, 2, 3);
-multiply(1, 2, 3, 4);
+
+
+
+console.log(add(15, 27));
+console.log(add(12, 4, 11, 48));
+console.log(add(32, 6, 13, 19, 8));
+console.log(add(74, 11, 62, 46, 12, 36));
+
+// 2
+
+// Change code below this line
+function addOverNum(threshold, ...args) {
+  let total = 0;
+
+  for (const  num  of args) {
+    if(num > threshold){
+      total += num;
+    }
+  }
+
+  return total;
+  // Change code above this line
+}
+
+console.log(addOverNum(50, 15, 27));
+console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
+console.log(addOverNum(15, 32, 6, 13, 19, 8));
+console.log(addOverNum(20, 74, 11, 62, 46, 12, 36));
+

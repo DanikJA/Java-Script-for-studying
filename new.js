ccacc8ec12c28884593fbf4a729435ce4f057e19
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-13 14:43:35 +0300

    made refactoring of the function from normal to arrow

--- new.js
+++ new.js
@@ -392,3 +392,11 @@ console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
 console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
 console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
 
+16.
+
+const calculateTotal = (quantity, pricePerItem) => {
+  // Change code above this line
+  return quantity * pricePerItem;
+}
+
+

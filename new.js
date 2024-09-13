0609c3cfc4ba88f24586a99fc843aa6cca8754fc
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-13 14:03:42 +0300

    used cycle forEach in the new interesting function

--- new.js
+++ new.js
@@ -346,3 +346,28 @@ function calculateTotalPrice(orderedItems) {
 console.log(calculateTotalPrice([12, 85, 37, 4]));
 console.log(calculateTotalPrice([164, 48, 291]));
 console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+
+// 14.
+
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  // Change code below this line
+
+ numbers.forEach(function(number){
+   if (number > value) {
+      filteredNumbers.push(number);
+    }
+ });
+    return filteredNumbers;
+  }
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+15.
+

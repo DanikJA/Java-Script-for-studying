d4e96a58b77f556258e6b6b456e125573e1e28b0
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-14 13:49:19 +0300

    added two interesting logic problems

--- new.js
+++ new.js
@@ -652,3 +652,60 @@ const oddNumbers = numbers.filter(number => number % 2 !== 0);
 
 console.log(evenNumbers);
 console.log(oddNumbers);
+
+// 27
+
+
+const bookss = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    genres: ["adventure", "history"],
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    genres: ["fiction", "mysticism"],
+  },
+  {
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    genres: ["horror", "mysticism", "adventure"],
+  },
+];
+// Change code below this line
+const allGenres = bookss.flatMap(book => book.genres);
+
+const uniqueGenres = allGenres.filter((genr, index, array) => array.indexOf(genr) === index);
+
+console.log(uniqueGenres);
+
+// 28.
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
+// // Change code below this line
+
+// const topRatedBooks = books.filter(book => book.rating >= MIN_RATING);
+// const booksByAuthor = books.filter(book => book.author === AUTHOR);
+

85b2af625cbd2f2673960ba39863d7766c72734d
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-16 21:20:35 +0300

    sorted arrays

--- new.js
+++ new.js
@@ -1916,3 +1916,21 @@ console.log(totallBalance);
 
 
 // 43
+
+const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+const authors = [
+  "Tanith Lee",
+  "Bernard Cornwell",
+  "Robert Sheckley",
+  "Fyodor Dostoevsky",
+];
+// Change code below this line
+
+const ascendingReleaseDates = [...releaseDates].sort();
+
+const alphabeticalAuthors = [...authors].sort();
+
+
+console.log(ascendingReleaseDates);
+console.log(alphabeticalAuthors);
+

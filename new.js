1609ac624f42bbf02cbf37559b67ed6ea6df3d8f
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-16 22:08:00 +0300

    counting years from lover to bigger

--- new.js
+++ new.js
@@ -1934,3 +1934,17 @@ const alphabeticalAuthors = [...authors].sort();
 console.log(ascendingReleaseDates);
 console.log(alphabeticalAuthors);
 
+
+// 44.
+
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// // Change code below this line
+
+// const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
+
+// const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
+
+// console.log(ascendingReleaseDates);
+// console.log(descendingReleaseDates);
+
+

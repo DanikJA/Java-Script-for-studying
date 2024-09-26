8875c109becbdf400525e361b8db4b30f6bce6fd
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-26 20:33:27 +0300

    ...

--- new.js
+++ new.js
@@ -2518,3 +2518,47 @@ console.log(audi.price); // 49000
 
 // console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
 // console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+
+
+// 55
+
+
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class Admin extends User {
+  // Change code below this line
+  constructor({email, accessLevel}){
+    super(email);
+    this.accessLevel = accessLevel;
+  }
+  static AccessLevel = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+
+  // Change code above this line
+}
+
+const mango = new Admin({
+  email: "mango@mail.com",
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"

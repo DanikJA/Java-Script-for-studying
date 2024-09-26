c0ac245b91cc001f83ff1ecd9c3fd661f689724f
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-26 17:54:46 +0300

    getter and setter

--- new.js
+++ new.js
@@ -2418,3 +2418,42 @@ storage.removeItem("Prolonger");
 console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
 
 
+// 54.
+
+// class Car {
+//   // Change code below this line
+//   #brand;
+//   #model;
+//   #price;
+
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+
+//   get brand() {
+//     return this.#brand;
+//   }
+
+//   set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//   get model() {
+//     return this.#model;
+//   }
+
+//   set model(newModel) {
+//     this.#model = newModel;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+//   // Change code above this line
+// }

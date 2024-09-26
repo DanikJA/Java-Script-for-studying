00d47ea1f22bc6c48246f4555a6ddb050374d1c0
Danik2023 <danik.kyrteu2@gmail.com>
2024-09-26 16:37:44 +0300

    added new code

--- new.js
+++ new.js
@@ -2329,3 +2329,35 @@ const user = new Car();
 //   // Change code above this line
 // }
 
+
+
+// class Storage {
+//   constructor(items){
+//     this.items = items;
+//   }
+//  getItems(){
+//    return this.items;
+//  } 
+
+//   addItem(newItem){
+//   this.items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove){
+//     const itemIndex = this.items.indexOf(itemToRemove);
+//     if(itemIndex !== -1){
+//       this.items.splice(itemIndex, 1)
+//     }
+//   }
+ 
+// }
+
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
